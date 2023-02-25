bd27677f04785ae56bcb86cf1cccf361d5b620a8
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-02-25 21:49:27 +0200

    homework_2

--- H_W.py
+++ H_W.py
@@ -7,11 +7,18 @@ user = {
 #print("|user_data|", user)
 #print("|user_friends|", user["friends"])
 print("|user_age|", user["age"])
+#user_age = int(user["age"])
+import datetime
+current_yaer = datetime.datetime.now().year
+print("yaer of user birth", int(current_yaer) - int(user["age"]))
 
-
+#user_age = input("How old are you ? ")
+#import datetime
+#current_yaer = datetime.datetime.now().year
+#print("year of your birth", int(current_yaer) - int(user_age))
 
 print("Mike" in user)
 
-if "Mike" in user,
+#if "Mike" in user,
 #    best_friend = "Mike"
-    print("Mike best_friend")
+#    print("Mike best_friend")
