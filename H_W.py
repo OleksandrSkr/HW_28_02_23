65364f1d7f49de89f71b86d441c9a258e2e63145
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-02-25 23:30:13 +0200

    homework_4

--- H_W.py
+++ H_W.py
@@ -4,21 +4,20 @@ user = {
     "friends": ["Mike", "Bob", "Joe"],
     "skills": ("HTML", "CSS", "JS"),
 }
-#print("|user_data|", user)
+print("|user_data|", user)
 #print("|user_friends|", user["friends"])
 print("|user_age|", user["age"])
-#user_age = int(user["age"])
 import datetime
 current_yaer = datetime.datetime.now().year
 print("yaer of user birth", int(current_yaer) - int(user["age"]))
 
-#user_age = input("How old are you ? ")
-#import datetime
-#current_yaer = datetime.datetime.now().year
-#print("year of your birth", int(current_yaer) - int(user_age))
+#print("Mike" in user)
 
-print("Mike" in user)
+if "Mike" in user["friends"]:
+    Mike = "best_friend"
+#    best_friend = ("Mike")
+#    print("best_friend")
+    print("Mike best_friend")
 
-#if "Mike" in user,
-#    best_friend = "Mike"
-#    print("Mike best_friend")
+user["skills"] = ["Python", "QA", "Selenium"]
+print("|user_data|", user)
