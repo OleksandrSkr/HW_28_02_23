12bd08a245ef5ff2f5216b60fdce11050cec0f63
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-02-25 21:33:39 +0200

    homework_1

---
+++ H_W.py
@@ -0,0 +1,17 @@
+user = {
+    "name": "John",
+    "age": 23,
+    "friends": ["Mike", "Bob", "Joe"],
+    "skills": ("HTML", "CSS", "JS"),
+}
+#print("|user_data|", user)
+#print("|user_friends|", user["friends"])
+print("|user_age|", user["age"])
+
+
+
+print("Mike" in user)
+
+if "Mike" in user,
+#    best_friend = "Mike"
+    print("Mike best_friend")
